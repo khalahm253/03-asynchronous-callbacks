e726aac015bbd0cb9ea59549eb84678daa56314b
khalahm253 <khalahm253@gmail.com>
2018-05-16 23:39:53 -0700

    initial commit

---
+++ __test__/reader.test.js
@@ -0,0 +1,38 @@
+'use strict';
+
+jest.mock('fs');
+
+const reader = require('../../lib/reader.js');
+
+describe('File Reader Module', () => {
+  
+  it('When given a bad file, returns an error', () => {
+    let path = `${__dirname}/../../data/bad.txt`;
+    reader(path, (err, data) => {
+      expect(err).toBeDefined();
+    });
+  });
+
+  it('When given a valid file path, returns string of text contained in file', () => {
+    let path = `${__dirname}/../../data/fine.txt`;
+    reader(path, (err, data) => {
+    expect(data).toBe('File Contents');
+    });
+  })
+  
+  it('When given an array of at least one invalid File paths, returns an error', () => {
+    let path = [`${__dirname}/../../data/bad.txt`, `${__dirname}/../../data/good.txt`, `${__dirname}/../../data/bad.txt`];
+    reader(path, (err) => {
+      expect(err).toBeDefined();
+    });
+  });
+
+  it('When given an array of valid File paths, returns an array of strings that contain text of each file', () => {
+    let path = [`${__dirname}/../../data/fine.txt`, `${__dirname}/../../data/good.txt`, `${__dirname}/../../data/okay.txt`, `${__dirname}/../../data/okay.txt`, `${__dirname}/../../data/okay.txt`, `${__dirname}/../../data/okay.txt` ];
+    reader(path, (err, data) => {
+      expect(data).toEqual(['File Contents', 'File Contents', 'File Contents', 'File Contents', 'File Contents', 'File Contents']);
+    });
+  });
+
+
+});
